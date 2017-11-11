c51e8e523a987cc11fd747f5879d8e06d44bedc6
nuklear0 <ene.bogd@gmail.com>
2017-11-11 13:16:49 +0100

    skeleton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MaterialModule } from './material.module';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { RollDicesComponent } from './roll-dices/roll-dices.component';
+import { InitiativeTrackerComponent } from './initiative-tracker/initiative-tracker.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RollDicesComponent,
+    InitiativeTrackerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialModule,
+    FormsModule
+  ],
+  exports: [
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/initiative-tracker/initiative-tracker.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-initiative-tracker',
+  templateUrl: './initiative-tracker.component.html',
+  styleUrls: ['./initiative-tracker.component.css'],
+  encapsulation: ViewEncapsulation.None
+})
+export class InitiativeTrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
