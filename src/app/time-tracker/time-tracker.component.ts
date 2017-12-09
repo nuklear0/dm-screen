6b481a8c2ea4563f8ed7faceff8a253594457ca7
nuklear0 <ene.bogd@gmail.com>
2017-12-09 14:33:48 +0100

    Added option to input custom time Styling fixes

--- src/app/time-tracker/time-tracker.component.ts
+++ src/app/time-tracker/time-tracker.component.ts
@@ -10,42 +10,62 @@ import {LocalStorageService} from 'angular-2-local-storage';
 export class TimeTrackerComponent implements OnInit {
 
   date: Date;
+  hour: number;
+  minute: number;
 
   constructor(private localStorageService: LocalStorageService) {
     if (!this.date) {
       if (this.localStorageService.get('time')) {
-        console.log(this.localStorageService.get('time'));
         this.date = new Date(Date.parse(this.localStorageService.get('time')));
       } else {
         this.date = new Date();
       }
     }
+    this.updateTime();
   }
 
   ngOnInit() {
   }
 
+  onChange(hourOrMinute: string) {
+    if (hourOrMinute === 'hour' && this.hour <= 23) {
+      this.date.setHours(this.hour);
+    } else if (hourOrMinute === 'minute' && this.minute <= 59) {
+      this.date.setMinutes(this.minute);
+    }
+    this.storeTime();
+    this.updateTime();
+  }
+
   addOneHour() {
     this.date.setHours(this.date.getHours() + 1);
     this.storeTime();
+    this.updateTime();
   }
 
   addHalf() {
     this.date.setMinutes(this.date.getMinutes() + 30);
     this.storeTime();
+    this.updateTime();
   }
 
   addEight() {
     this.date.setHours(this.date.getHours() + 8);
     this.storeTime();
+    this.updateTime();
   }
 
   storeTime() {
-    console.log('storing ', this.date);
     this.localStorageService.add('time', this.date.toString());
+  }
+
+  updateTime() {
+    this.hour = this.date.getHours();
+    this.minute = this.date.getMinutes();
   }
 
   resetTime() {
     this.date = new Date();
+    this.updateTime();
   }
 }
